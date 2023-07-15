b016a2f8a69e66e4fec6666dfcf186b45d79bf28
Materxyz <139655426+Materxyz@users.noreply.github.com>
2023-07-16 05:02:18 +0700

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -12,12 +12,12 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
                     level=logging.WARNING)
 
 # variables
-API_ID = config("API_ID", default=None, cast=int)
-API_HASH = config("API_HASH", default=None)
-BOT_TOKEN = config("BOT_TOKEN", default=None)
-SESSION = config("SESSION", default=None)
-FORCESUB = config("FORCESUB", default=None)
-AUTH = config("AUTH", default=None, cast=int)
+API_ID = 23532861
+API_HASH = "6f0cfb9808100e072b7dab1d6b9ee3c8"
+BOT_TOKEN = "6259477626:AAF7YdZR1q4kGnS0dAKe7GSISJEpmqXjjqw"
+SESSION = "BQFnFT0AEOOLRK1eMzS3oK7uHH4sjAcKWDHQ02zY_i0Us5e95XO8Zyx5XvYTfAGGTWGRIHM9QjBM_gdFQubvhPwx1U_NKYOG4Dy2MBYGen3hIzy6JzHB44roE_9Zl7mEylFSyA0TR6rmVnwX_57TaKadMNc_RqiHVoWbtXTVmC1_jNziA0PNoAtLp6MSkGiy5BgHuKSfzNIr6JQr6i5IFDcpDib3bkoVbiaFihXF8nouRIFssPxJNZ29uQEX491WjzP9Oq2Y8fvqzzOjcpH6lzBqjwYiDU4LP_ZVFT1fRCSpRW8q1X_ZPqQvNwlvPkOALq_mV2Ym_NrKEKub3r9nTajFxAlFOgAAAAAUw-IZAA"
+FORCESUB = "SaveR_estrictedContentBot"
+AUTH = "MShaun"
 
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
 
